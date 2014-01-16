7511a2ed92ee368b8d455398f9625e900f64f541
Mark Reeves <mjr@clearbold.com>
2014-01-16 16:37:23 -0500

    Added some more comments to example.config.php

--- app/config/example.config.php
+++ app/config/example.config.php
@@ -5,10 +5,10 @@ $send_html = False; // Set to True to use the HTML template and send multipart
 
 $default_timezone = 'America/New_York'; // http://us2.php.net/manual/en/timezones.america.php
 
-$default_sender = 'you@yourdomain.com';
-$default_sender_name = '';
+$default_sender = 'you@yourdomain.com'; // Prepopulates the form
+$default_sender_name = 'Your Name'; // Prepopulates the form
 
-$smtp_server = '';
+$smtp_server = ''; // I use https://mandrillapp.com for these
 $smtp_port = '587';
 $smtp_username = '';
 $smtp_password = '';
